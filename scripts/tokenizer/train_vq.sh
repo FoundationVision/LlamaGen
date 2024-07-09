7b7e3aac0026b7ec6aa95bcd391f6f13d3adf979
Ryankwon03 <ryankwon@umich.edu>
2024-07-09 19:07:18 +0900

    Update train_vq.sh

--- scripts/tokenizer/train_vq.sh
+++ scripts/tokenizer/train_vq.sh
@@ -1,6 +1,16 @@
 # !/bin/bash
 set -x
 
+# Set default values for single-node training
+nnodes=1                  # number of nodes
+nproc_per_node=1          # number of processes per node
+node_rank=0               # node rank
+master_addr="localhost"   # address of the master node
+master_port=29500         # port of the master node
+
+
+
+
 torchrun \
 --nnodes=$nnodes --nproc_per_node=$nproc_per_node --node_rank=$node_rank \
 --master_addr=$master_addr --master_port=$master_port \
