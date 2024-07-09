8533bb758e76fcffd5a12012a75f145c2e46ab39
Ryan Sungmo Kwon <ryankwon@umich.edu>
2024-07-09 18:54:34 +0900

    Update train_vq.sh

--- scripts/tokenizer/train_vq.sh
+++ scripts/tokenizer/train_vq.sh
@@ -1,6 +1,13 @@
 # !/bin/bash
 set -x
 
+nnodes=1
+nproc_per_node=1
+node_rank=0
+master_addr="127.0.0.1"
+master_port=29500
+
+
 torchrun \
 --nnodes=$nnodes --nproc_per_node=$nproc_per_node --node_rank=$node_rank \
 --master_addr=$master_addr --master_port=$master_port \
